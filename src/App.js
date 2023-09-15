0abcedf77e73c7b6b61932642e638dd1b499917d
tliontube <thomaswizfan@gmail.com>
2023-09-15 13:12:55 +0530

    v2

--- src/App.js
+++ src/App.js
@@ -1,36 +1,44 @@
-import Footer from "./Components/Footer";
+import React, { useState } from "react";
 import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 import Note from "./Components/Note";
-import notes from "./notes";
+import CreateArea from "./Components/CreateArea";
+
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div>
-      <Header/>
-      {notes.map((noteItem)=>{
-        return(
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
           <Note
-            key={noteItem.key}
+            key={index}
+            id={index}
             title={noteItem.title}
             content={noteItem.content}
+            onDelete={deleteNote}
           />
-        )
+        );
       })}
-      <Footer/>
+      <Footer />
     </div>
   );
 }
 
 export default App;
-
-//1. Create a new React app.
-//2. Create a App.jsx component.
-//3. Create a Header.jsx component that renders a <header> element
-//to show the Keeper App name in an <h1>.
-//4. Create a Footer.jsx component that renders a <footer> element
-//to show a copyright message in a <p> with a dynamically updated year.
-//5. Create a Note.jsx component to show a <div> element with a
-//<h1> for a title and a <p> for the content.
-//6. Make sure that the final website is styled like the example shown here:
-//https://l1pp6.csb.app/
-
-//HINT: You will need to study the classes in teh styles.css file to appy styling.
