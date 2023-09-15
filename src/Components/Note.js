5d630124d3a27fcaba9dbda970085dd2a7c52f39
tliontube <thomaswizfan@gmail.com>
2023-09-15 15:39:07 +0530

    finalBuild

--- src/Components/Note.js
+++ src/Components/Note.js
@@ -1,11 +1,17 @@
+import React from "react";
 
-function note(props){
-    return(
-        <div className="note">
-            <h1>{props.title}</h1>
-            <p>{props.content}</p>
-        </div>
-    )
+function Note(props) {
+  function handleClick() {
+    props.onDelete(props.id);
+  }
+
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+      <button onClick={handleClick}>DELETE</button>
+    </div>
+  );
 }
 
-export default note;
+export default Note;
